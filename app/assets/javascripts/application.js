3cb01e1b3bb228bf1e9ae0929b747f5b7ff8cd9f
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-07-15 22:19:13 +0700

    Add facebook messenger widget

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,20 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)){
+    return;
+  }
+  js = d.createElement(s); 
+  js.id = id;
+  js.src = '//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+$(function(){
+  $(".chat_fb").click(function() {
+    $('.fchat').toggle('slow');
+  });
+});
