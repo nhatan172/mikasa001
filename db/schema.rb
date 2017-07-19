79da6b911ac8fb7ff1b241de6c6d5f4d1f0afaa3
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-07-19 12:41:51 +0700

    Change article model schema

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 20170718015944) do
   create_table "articles", force: :cascade do |t|
     t.integer  "doc_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "html_content"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
